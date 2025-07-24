83d8e9cc94f32cf403f70b9e31477404aa8fc805
Sarah <skat1403@gmail.com>
2025-07-24 03:13:53 -0500

    init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+function calculateDiscount(price, discountRate) {
+    if (typeof price !== 'number' || typeof discountRate !== 'number') return null;
+    if (discountRate < 0 || discountRate > 1) return null;
+    else return  price - (price*discountRate);
+    
+}
+
+function filterProducts(products, callback) {
+    if (!Array.isArray(products) || typeof callback !== 'function') return [];
+    else return products.filter(callback);
+}
+
+function sortInventory(inventory, key) {
+    if (!Array.isArray(inventory) || typeof key !== 'string') return [];
+    else return inventory.sort((a, b) => a.key - b.key);
+}
+
+console.log(calculateDiscount(100, .4))
+
+module.exports = {calculateDiscount, filterProducts, sortInventory};
